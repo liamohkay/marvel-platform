5a8e8a8d6835cd49254cca4088556b4d9bb2e200
poojithreddy28 <poojithannachedu@gmail.com>
2025-01-22 00:47:34 -0600

    feat: create Blockquote,Heading,Basicmarks plugins

--- components/editor/plate-editor.jsx
+++ components/editor/plate-editor.jsx
@@ -2,7 +2,21 @@
 
 import React, { useState } from 'react';
 
-import { Plate, usePlateEditor } from '@udecode/plate/react';
+import { withProps } from '@udecode/cn';
+import {
+  Plate,
+  PlateElement,
+  PlateLeaf,
+  usePlateEditor,
+} from '@udecode/plate/react';
+import {
+  BoldPlugin,
+  CodePlugin,
+  ItalicPlugin,
+  UnderlinePlugin,
+} from '@udecode/plate-basic-marks/react';
+import { BlockquotePlugin } from '@udecode/plate-block-quote/react';
+import { HeadingPlugin } from '@udecode/plate-heading/react';
 import { Editor, EditorContainer } from '@/components/plate-ui/editor';
 
 const initialValue = [
@@ -23,6 +37,42 @@ export function PlateEditor({ markdownContent }) {
     typeof window !== 'undefined' && localStorage.getItem('editorContent');
 
   const editor = usePlateEditor({
+    override: {
+      components: {
+        blockquote: withProps(PlateElement, {
+          as: 'blockquote',
+          className: 'mb-4 border-l-4 border-[#d0d7de] pl-4 text-[#636c76]',
+        }),
+        bold: withProps(PlateLeaf, { as: 'strong' }),
+        h1: withProps(PlateElement, {
+          as: 'h1',
+          className:
+            'mb-4 mt-6 text-3xl font-semibold tracking-tight lg:text-4xl',
+        }),
+        h2: withProps(PlateElement, {
+          as: 'h2',
+          className: 'mb-4 mt-6 text-2xl font-semibold tracking-tight',
+        }),
+        h3: withProps(PlateElement, {
+          as: 'h3',
+          className: 'mb-4 mt-6 text-xl font-semibold tracking-tight',
+        }),
+        italic: withProps(PlateLeaf, { as: 'em' }),
+        p: withProps(PlateElement, {
+          as: 'p',
+          className: 'mb-4',
+        }),
+        underline: withProps(PlateLeaf, { as: 'u' }),
+      },
+    },
+    plugins: [
+      BlockquotePlugin,
+      HeadingPlugin,
+      BoldPlugin,
+      ItalicPlugin,
+      UnderlinePlugin,
+      CodePlugin,
+    ],
     value: localValue ? JSON.parse(localValue) : initialValue,
   });
 
@@ -35,7 +85,11 @@ export function PlateEditor({ markdownContent }) {
       }}
     >
       <EditorContainer>
-        <Editor />
+        <Editor
+          placeholder="Type here..."
+          autoFocus={false}
+          spellCheck={false}
+        />
       </EditorContainer>
       
       {/* Debugging Section */}
