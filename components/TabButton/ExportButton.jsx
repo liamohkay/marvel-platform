bcd0c1d7e438a3f6496fc4dd5487973dbd5cb5cb
Derinsola Obasa <derin.obasa@gmail.com>
2025-01-25 01:37:08 -0600

    Implementation updates

--- components/TabButton/ExportButton.jsx
+++ components/TabButton/ExportButton.jsx
@@ -1,25 +1,32 @@
 import React, { useState } from 'react';
 
-import IosShareIcon from '@mui/icons-material/IosShare';
+// import IosShareIcon from '@mui/icons-material/IosShare';
 import { saveAs } from 'file-saver';
 import { jsPDF } from 'jspdf';
+import { useSelector } from 'react-redux';
 
-const ExportButton = ({ content }) => {
+const ExportButton = () => {
   const [showDropdown, setShowDropdown] = useState(false);
+
+  const markdownContent = useSelector(
+    (state) => state.tools.editorState.markdownContent
+  );
 
   const handleExportPDF = () => {
     const doc = new jsPDF();
-    doc.text(content, 10, 10);
+    doc.text(markdownContent, 10, 10);
     doc.save('document.pdf');
   };
 
   const handleExportText = () => {
-    const blob = new Blob([content], { type: 'text/plain;charset=utf-8"#' });
+    const blob = new Blob([markdownContent], {
+      type: 'text/plain;charset=utf-8"#',
+    });
     saveAs(blob, 'document.txt"#');
   };
 
   const handleExportHTML = () => {
-    const blob = new Blob([`<html><body>${content}</body></html>`], {
+    const blob = new Blob([`<html><body>${markdownContent}</body></html>`], {
       type: 'text/html;charset=utf-8',
     });
     saveAs(blob, 'document.html');
@@ -28,7 +35,36 @@ const ExportButton = ({ content }) => {
   return (
     <div className="export-container">
       <button type="button" onClick={() => setShowDropdown(!showDropdown)}>
-        {IosShareIcon}Export
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M7.38948 8.98354H6.45648C4.42148 8.98354 2.77148 10.6335 2.77148 12.6685V17.5435C2.77148 19.5775 4.42148 21.2275 6.45648 21.2275H17.5865C19.6215 21.2275 21.2715 19.5775 21.2715 17.5435V12.6585C21.2715 10.6295 19.6265 8.98354 17.5975 8.98354L16.6545 8.98354"
+            stroke="#9E7EFF"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M12.0215 2.19044V14.2314"
+            stroke="#9E7EFF"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M9.10645 5.11816L12.0214 2.19016L14.9374 5.11816"
+            stroke="#9E7EFF"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+        Export
       </button>
 
       {showDropdown && (
