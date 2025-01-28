e9b16e8ce2919808fab3f63b69e9a5e47f144c5b
Derinsola Obasa <derin.obasa@gmail.com>
2025-01-27 22:34:36 -0600

    Export Button styles implementation

--- components/TabButton/ExportButton.jsx
+++ components/TabButton/ExportButton.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { saveAs } from 'file-saver';
 import { jsPDF } from 'jspdf';
 import { useSelector } from 'react-redux';
+import styles from './ExportButton.module.css';
 
 const ExportButton = () => {
   const [showDropdown, setShowDropdown] = useState(false);
@@ -33,49 +34,63 @@ const ExportButton = () => {
   };
 
   return (
-    <div className="export-container">
-      <button type="button" onClick={() => setShowDropdown(!showDropdown)}>
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M7.38948 8.98354H6.45648C4.42148 8.98354 2.77148 10.6335 2.77148 12.6685V17.5435C2.77148 19.5775 4.42148 21.2275 6.45648 21.2275H17.5865C19.6215 21.2275 21.2715 19.5775 21.2715 17.5435V12.6585C21.2715 10.6295 19.6265 8.98354 17.5975 8.98354L16.6545 8.98354"
-            stroke="#9E7EFF"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M12.0215 2.19044V14.2314"
-            stroke="#9E7EFF"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M9.10645 5.11816L12.0214 2.19016L14.9374 5.11816"
-            stroke="#9E7EFF"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-        Export
-      </button>
+    <div>
+      <div className={styles.buttonContainer}>
+        <button type="button" onClick={() => setShowDropdown(!showDropdown)}>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M7.38948 8.98354H6.45648C4.42148 8.98354 2.77148 10.6335 2.77148 12.6685V17.5435C2.77148 19.5775 4.42148 21.2275 6.45648 21.2275H17.5865C19.6215 21.2275 21.2715 19.5775 21.2715 17.5435V12.6585C21.2715 10.6295 19.6265 8.98354 17.5975 8.98354L16.6545 8.98354"
+              stroke="#9E7EFF"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M12.0215 2.19044V14.2314"
+              stroke="#9E7EFF"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M9.10645 5.11816L12.0214 2.19016L14.9374 5.11816"
+              stroke="#9E7EFF"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <span>Export</span>
+        </button>
+      </div>
 
       {showDropdown && (
-        <div>
-          <button type="button" onClick={handleExportPDF}>
+        <div className={styles.dropdown}>
+          <button
+            className={styles.dropdownTex}
+            type="button"
+            onClick={handleExportPDF}
+          >
             Export as PDF
           </button>
-          <button type="button" onClick={handleExportText}>
+          <button
+            className={styles.dropdownTex}
+            type="button"
+            onClick={handleExportText}
+          >
             Export as Text
           </button>
-          <button type="button" onClick={handleExportHTML}>
+          <button
+            className={styles.dropdownTex}
+            type="button"
+            onClick={handleExportHTML}
+          >
             Export as HTML
           </button>
         </div>
